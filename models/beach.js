8f9ac45926de8006d2f2ecd1fd49735a1866223c
mpham-12 <martinp0512@gmail.com>
2022-03-03 22:05:20 -0500

    delete all reviews from db when a beach is deleted

--- models/beach.js
+++ models/beach.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const BeachSchema = new Schema({
   title: String,
@@ -14,6 +15,16 @@ const BeachSchema = new Schema({
       ref: 'Review'
     }
   ]
+})
+
+BeachSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+      await Review.deleteMany({
+          _id: {
+              $in: doc.reviews
+          }
+      })
+  }
 })
 
 module.exports = mongoose.model('Beach', BeachSchema);
