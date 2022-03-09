e9636e34e965f7ccc31dd5f0dae3cc328256e56f
mpham-12 <martinp0512@gmail.com>
2022-03-09 13:39:37 -0500

    config cloudinary

--- routes/beaches.js
+++ routes/beaches.js
@@ -4,7 +4,8 @@ const catchAsync = require('../helpers/catchAsync');
 const { isAuthor, isLoggedIn, isOwner, validateReview, validateBeach } = require('../helpers/middleware');
 const beaches = require('../controllers/beaches');
 const multer = require('multer'); //parses enctype="multipart/form-data"
-const upload = multer({ dest: 'uploads/' })
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 //index
 router.get('/', catchAsync(beaches.index))
@@ -14,7 +15,7 @@ router.get('/new', isLoggedIn, beaches.newForm)
 
 //post
 // router.post('/', isLoggedIn, validateBeach, catchAsync(beaches.postBeach))
-router.post('/', upload.single('image'), (req,res)=>{
+router.post('/', upload.single('image'), (req, res) => {
   console.log('req.body----', req.body, 'req.file-----', req.file);
   res.send('it worked')
 
