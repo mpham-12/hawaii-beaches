d3c2b5b64a1e9fc3cde8c07d19220fcbf3ea6430
mpham-12 <martinp0512@gmail.com>
2022-03-11 19:22:20 -0500

    change path to mongo atlas

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const MongoStore = require("connect-mongo")(session);
-// const dbUrl= process.env.DB_URL || 'mongodb://localhost:27017/hawaii-beaches';
-const dbUrl= 'mongodb://localhost:27017/hawaii-beaches';
+const dbUrl= process.env.DB_URL || 'mongodb://localhost:27017/hawaii-beaches';
+// const dbUrl= 'mongodb://localhost:27017/hawaii-beaches';
 
 
 
