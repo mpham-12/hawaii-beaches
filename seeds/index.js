f60621b0ffec62770cf97489e6c14589fe524781
mpham-12 <martinp0512@gmail.com>
2022-03-14 14:28:13 -0400

    edit owner id in seeds

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.DB_URL, {
 //beaches seed
 const seedBeaches = [
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Sunset Beach',
     description: 'One of the three world-renown surfing beaches on the North Shore (Banzai and Waimea are the other two), Sunset Beach stretches two miles from Sunset point to Ehukai beach. It’s an excellent spot to pull up a chair and watch surfers during the winter.',
     location: 'Pupukea, Hawaii',
@@ -30,7 +30,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Ehukai Beach',
     description: 'Besides the beautiful beige, wide-sandy beach, Banzai offers surfing spectators one of the most unique waves in the world, the infamous Banzai Pipeline, known for the way the waves break over a sharp and shallow reef into massive tubes (winter season).',
     location: 'Pupukea, Hawaii',
@@ -44,7 +44,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Waimea Bay',
     description: 'Waimea Bay is large and horseshoe-shaped, perfect for a lazy summer afternoon of swimming and sunbathing. It was the birthplace of big wave surfing in Hawai‘i, and during the winter, the bay brings in waves that top 30 feet in height.',
     location: 'Waimea, Hawaii',
@@ -58,7 +58,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Haleiwa Beach',
     description: 'Haleiwa is one of the beaches with calmer waters on the north shore. Try here if the winter surf is too unsafe for swimming at other places along the northern coast. It’s also a great place to take a surf lesson.',
     location: 'Haleiwa, Hawaii',
@@ -72,7 +72,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Chuns Reef Beach',
     description: 'Less trafficked than Waimea or Haleiwa, Chun’s offers a glimpse of “everyday” surf culture – that is, daily surf life outside of the world-famous breaks – and an off-beat place to put your feet up. Its long stretch of white sand makes it good for walking, and a freshwater pond is the perfect place for families with small children.',
     location: 'Haleiwa, Hawaii',
@@ -86,7 +86,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Turtle Bay Beach',
     description: 'These side by side bays are more protected from waves than other North Shore beaches, making them good for snorkeling and swimming year round, even when the surf is big at other places along the North Shore. Kewela Bay Beach Park offers plenty of shade and a thick forest ironwood trees to complement its crystal blue waters and fine white sand.',
     location: 'Kawela, Hawaii',
@@ -100,7 +100,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Lanikai Beach',
     description: 'Lanikai has been ranked amongst the best beaches in the world, and it’s easy to see why when you arrive. Powdery white sand, clear blue water, and a vantage point of two offshore islands, known locally as “the Mokes.” The water here is as calm as it gets.',
     location: 'Kailua, Hawaii',
@@ -114,7 +114,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Pupukea Beach',
     description: ' Pūpūkea Beach Park is home to “Shark’s Cove,” one of the best areas for snorkeling and diving during the summer. A large protected cove is idea for families and beginner swimmers. Outside the cove, lava tubes, reef walls, and wildlife attract snorkelers, free divers, and scuba divers.',
     location: 'Pupukea, Hawaii',
@@ -128,7 +128,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Waimanalo Beach',
     description: 'Waimanalo Beach is one of the longest in Hawai‘i, offering several miles of white sand to walk. It’s not overly rough, but it does get mild waves along the shore, fun for active swimming. Popular with locals for fishing, barbecuing, and family hang outs.',
     location: 'Waimanalo Beach, Hawaii',
@@ -142,7 +142,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Kailua Beach',
     description: 'Kailua Beach is similar to Waimanalo in that it is long, with fine white sand and reasonably calm waters. It has views of several offshore islands and has a lot of activities going on, including kayaking, kite surfing, wind surfing, fishing, and boating.',
     location: 'Kailua, Hawaii',
@@ -156,7 +156,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Makapuu Beach',
     description: 'Backed by the tall cliffs of the Koʻolau Volcano, the surf is often large at Makapuʻu, attracting a youthful crowd of bodyboarders and beachgoers. You can see two islands off the coast, as well as the tall cliffs that support the Makapuʻu Lighthouse',
     location: 'Waimanalo Beach, Hawaii',
@@ -170,7 +170,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Kahana Bay Beach',
     description: ' Completely out of the way on the upper windwardside, Kahana remains a nice local hangout, boxed in by tall, jagged cliffs and a large, horseshoe bay. It’s not a great beach for swimming due to the runoff from the mountains, but it’s a good place to hang out, stand up paddleboard, or walk the trails opposite the beach.',
     location: 'Hauula, Hawaii',
@@ -184,7 +184,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Malaekahana Beach',
     description: 'This state park is popular with locals for camping and fishing. Beaches here are small but swimming is good in summer.',
     location: 'Koolauloa, Hawaii',
@@ -198,7 +198,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Hanauma Bay',
     description: 'Great snorkeling! Sure, the beach is also nice for sunbathing, but Hanauma, with its entry fee and reef safety video (required for entrance), is all about exploring the underwater world.',
     location: 'Hanauma, Hawaii',
@@ -212,7 +212,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Waikiki Beach',
     description: 'The iconic beach of Hawai‘i, with Diamond Head looming overhead. One of the most well-known beaches in the world and a legendary place to learn to surf.',
     location: 'Waikiki, Hawaii',
@@ -226,7 +226,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Magic Island Lagoon',
     description: 'Magic Island is a man-made peninsula with large seawalls and a shallow lagoon, the latter a nice place for small children to play.',
     location: 'Honolulu, Hawaii',
@@ -240,7 +240,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Halona Beach Cove',
     description: 'Halona Beach Cove is one of the smallest beaches on O‘ahu, offering a cozy setting between two rock outcroppings. Swimming is possible on very calm days, but it’s mostly rocky, so not recommended. It can be easily reached via a short walk down from the Halona Blowhole overlook. The cove was made famous by the movie From Here to Eternity, filmed there in 1953.',
     location: 'Honolulu, Hawaii',
@@ -254,7 +254,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Sandy Beach',
     description: 'Just east of Halona Beach Cove is Sandy’s beach. Featuring a number of surf breaks and one of the largest shore breaks on the island, Sandy’s is a hub of activity and youth. Great for on-lookers, body boarders and surfers frequent this beach, so pull up a chair and hang out for a while. Check with the lifeguards before entering the water, as the conditions are often rough and dangerous.',
     location: 'Honolulu, Hawaii',
@@ -268,7 +268,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Diamond Head Beach',
     description: 'Diamond Head Beach requires a long walk down a steep cliff to reach, but it’s typically very relaxed and uncrowded, at least on the beach (the water can get crowded with surfers). There’s not great swimming here due to the rocks, but it’s a nice beach for a walk or to find a slice of sand.',
     location: 'Honolulu, Hawaii',
@@ -282,7 +282,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Ewa Beach',
     description: 'Ewa Beach is a long, narrow beach that covers most of the southwest coast of the island. Along it you will find a five-acre beach park (Ewa Beach) and a beginner surf break (White Plains). Families will love the green space, picnic areas, and residential vibe at Ewa Beach proper; surfers will enjoy the small, beginner-friendly waves at White Plains.',
     location: 'Ewa Beach, Hawaii',
@@ -296,7 +296,7 @@ const seedBeaches = [
   },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Depot Beach',
     description: 'Adjacent to Nanakuli, this long stretch of white sand is a local hangout for fishing and barbecuing.',
     location: 'Nanakuli, Hawaii',
@@ -310,7 +310,7 @@ const seedBeaches = [
     },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Makaha Beach',
     description: 'If you’re looking for surf culture on the west side, check out Makaha, home to another world-renown break. It’s not recommended for all swimmers, as the undertow can get quite strong. But, it’s a great place to post up and watch the surf from shore.',
     location: 'Makaha, Hawaii',
@@ -324,7 +324,7 @@ const seedBeaches = [
     },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Keawaula Beach',
     description: 'A steep-sloped beach with turquoise waters, Yokohama, or “Yokes” as it is called by locals, is a great place to snorkel when it’s calm, thanks to big rocks along the ocean entrance. Locals like to barbecue here.',
     location: 'Waialua, Hawaii',
@@ -338,7 +338,7 @@ const seedBeaches = [
     },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Pokai Bay Beach',
     description: 'If you are searching for a beach where you can swim, snorkel, and be one of the only visitors, head out to Pokai Bay and enjoy this secluded shoreline. It is surrounded by a reef, so the waters are calm and children can go snorkeling.',
     location: 'Waianae, Hawaii',
@@ -352,7 +352,7 @@ const seedBeaches = [
     },
   },
   {
-    owner: '6227c3244b715ad7502b1034',
+    owner: '622bda19d99f654db0f26864',
     title: 'Bellows Field Beach',
     description: 'Well-known for its soft sand and beautiful scenery, Bellows Field Beach Park is mostly visited by body surfers. The breaks here are definitely smaller than on Sandy Beach, so the waters can be shallow, making it popular with families with young children.',
     location: 'Waimanalo, Hawaii',
